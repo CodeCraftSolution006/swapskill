c6d296ea8d8b82d1bb05012d0fe5cf1f54479732
Sachal <mahersachal006@gmail.com>
2025-11-16 21:55:33 +0500

    Fix Vercel deployment: remove vercel-wsgi, add proper build configuration

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,15 +1,9 @@
 import os
 import django
 
-from vercel_wsgi import handle_wsgi_request
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-
-def handler(request, context):
-    """Vercel handler that adapts Django WSGI app to Vercel's Python runtime."""
-    return handle_wsgi_request(application, request, context)
